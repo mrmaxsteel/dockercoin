50a409fab33365248cbeb12336add9fc801367db
Max <max@VSiMac2.connect>
2018-08-29 09:59:10 +0100

    add_address

---
+++ node/node.py
@@ -0,0 +1,20 @@
+import datetime
+
+
+class DCNode:
+    """This class represents the DockerCoin Node"""
+    _node_address = ''
+    address_list = []
+
+    def __init__(self, address):
+        self._node_address = address
+        self.add_address(address)
+        self.add_address(address)
+
+    def add_address(self, address):
+        if True:
+            self.address_list.append({ 'addr': address, 'timestamp': str(datetime.datetime.now())})
+
+    def ping_address(self, address):
+        return True
+    
--- node/run.py
+++ node/run.py
@@ -1,17 +1,16 @@
 # encoding=utf-8
 
 from flask import Flask, render_template
-import node
+from node import DCNode
+import os
 
 app = Flask(__name__)
+dc_node = DCNode(str(os.environ['NODE_ADDRESS']))
 
 @app.route("/dashboard")
 def dashboard():
-    return render_template("index.html", address_list=node.address_list)
+    return render_template("index.html", address_list=dc_node.address_list)
 
 @app.route("/getaddr")
 def gettaddr():
-    return node.address_list[0]['addr']
-
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    return dc_node.address_list[0]['addr']
